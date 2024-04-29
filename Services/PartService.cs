cdc221cc041d7d8e1ebd7018083f6e09bd67ca07
Azat2k <dzhanov.azat@gmail.com>
2024-04-29 00:30:05 -0500

    Added validation in the Part interface implementation.

--- Services/PartService.cs
+++ Services/PartService.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RepairShopV1.Data;
 using RepairShopV1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace RepairShopV1.Services
 {
@@ -12,27 +16,51 @@ namespace RepairShopV1.Services
         {
             _context = context;
         }
-        
+
         public async Task<List<Part>> GetAllAsync()
         {
             return await _context.Parts.ToListAsync();
         }
+
         public async Task<Part> GetByIdAsync(int Id)
         {
             return await _context.Parts.FindAsync(Id);
         }
+
         public async Task<Part> AddAsync(Part part)
         {
+            if (part == null)
+            {
+                throw new ArgumentNullException(nameof(part));
+            }
+
+            if (!ValidatePart(part))
+            {
+                throw new ValidationException("The part model is not valid.");
+            }
+
             _context.Parts.Add(part);
             await _context.SaveChangesAsync();
             return part;
         }
+
         public async Task<Part> UpdateAsync(Part part)
         {
+            if (part == null)
+            {
+                throw new ArgumentNullException(nameof(part));
+            }
+
+            if (!ValidatePart(part))
+            {
+                throw new ValidationException("The part model is not valid.");
+            }
+
             _context.Entry(part).State = EntityState.Modified;
             await _context.SaveChangesAsync();
             return part;
         }
+
         public async Task DeleteAsync(int Id)
         {
             var part = await _context.Parts.FindAsync(Id);
@@ -41,6 +69,14 @@ namespace RepairShopV1.Services
                 _context.Parts.Remove(part);
                 await _context.SaveChangesAsync();
             }
-        }  
+        }
+
+        // Метод для валидации модели Part
+        private bool ValidatePart(Part part)
+        {
+            var validationContext = new ValidationContext(part);
+            var validationResults = new List<ValidationResult>();
+            return Validator.TryValidateObject(part, validationContext, validationResults, true);
+        }
     }
 }
