59ecd2b5db673045f6c963f93e7f17c15906740d
Azat2k <dzhanov.azat@gmail.com>
2024-05-01 23:24:43 -0600

    .

--- Program.cs
+++ Program.cs
@@ -12,9 +12,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
 builder.Services.AddScoped<IPartService, PartService>();//DI
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
